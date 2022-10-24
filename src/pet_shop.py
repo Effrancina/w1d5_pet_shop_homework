8254fc501ddb8fcb16dcff1c2a89741bdd5549e6
Francesca Menichetti <francescamenichetti@Francescas-MacBook-Pro.local>
2022-10-24 13:38:23 +0100

    test

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -39,12 +39,20 @@ def get_stock_count(pet_shop):
 # go through each pet's breed
 # if the breed found is equal to our parameter, add it to same_breed
 # return length of same breed
+# My solution:
+# def get_pets_by_breed(pet_shop, breed_to_find):
+#     same_breed = []
+#     for pet in pet_shop["pets"]:
+#         if pet["breed"] == breed_to_find:
+#             same_breed.append(pet["breed"])
+#     return same_breed
 
+# Correct solution:
 def get_pets_by_breed(pet_shop, breed_to_find):
     same_breed = []
     for pet in pet_shop["pets"]:
         if pet["breed"] == breed_to_find:
-            same_breed.append(pet["breed"])
+            same_breed.append(pet)
     return same_breed
 
 # def find_pet_by_name(pet_shop, pet_x):
@@ -106,23 +114,26 @@ def add_pet_to_customer(customer, new_pet):
 
 
 # OPTIONAL
+
+# My solution: (it works, it's ok)
+# def customer_can_afford_pet(customer, new_pet):
+#     if customer["cash"] >= new_pet["price"]:
+#         return True
+
+# def customer_can_afford_pet(customer, new_pet):
+#     if customer["cash"] >= new_pet["price"]:
+#         return True
+#     else:
+#         return False
+
+# def customer_can_afford_pet(customer, new_pet):
+#     if customer["cash"] >= new_pet["price"]:
+#         return True
+#     elif customer["cash"] == new_pet["price"]:    WE ACTUALLY DON'T NEED THE ELIF, CAUSE EQUAL IS CONTAINED IN THE PREVIOUS IF STATEMENT
+#         return True
+#     else:
+#         return False
+
+# Solutions:
 def customer_can_afford_pet(customer, new_pet):
-    if customer["cash"] >= new_pet["price"]:
-        return True
-
-def customer_can_afford_pet(customer, new_pet):
-    if customer["cash"] >= new_pet["price"]:
-        return True
-    else:
-        return False
-
-def customer_can_afford_pet(customer, new_pet):
-    if customer["cash"] >= new_pet["price"]:
-        return True
-    elif customer["cash"] == new_pet["price"]:
-        return True
-    else:
-        return False
-
-
-    
+    return customer["cash"] >= new_pet["price"]
